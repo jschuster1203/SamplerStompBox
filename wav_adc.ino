2d8b4d0f39934fdba52054673b27fe86c57033bb
jschuster1203 <54950513+jschuster1203@users.noreply.github.com>
2022-01-29 14:40:04 -0500

    Switched to stackoverflow code for testing

--- wav_adc.ino
+++ wav_adc.ino
@@ -34,7 +34,7 @@ uint16_t audioFormat = 1;
 ///: Mono = 1, Stereo = 2, etc.
 uint16_t numChannels = 1;
 ///: Sample Rate of file
-uint32_t sampleRates = 48827;
+uint32_t sampleRates = 48827; //TEST THIS VALUE TO GET MORE ACCURATE SAMPLERATE
 ///: SampleRate * NumChannels * BitsPerSample/8
 uint32_t byteRate = 48827;
 ///: The number of byte for one frame NumChannels * BitsPerSample/8
@@ -58,26 +58,22 @@ void setup()
   if (!SD.begin(BUILTIN_SDCARD)){
     Serial.println("initialization failed!");
     return;}
-    //while (1);
+  
 
-  wavFile = SD.open("DA1.txt", FILE_WRITE);
+  wavFile = SD.open("DA1.wav", FILE_WRITE);
 
-
-//  if (!wavFile)
-//    while (1);
 /* maybe change this section to different settings to get easier data to read*/
   adc->adc0->setAveraging(32);
-  adc->adc0->setResolution(10);
+  adc->adc0->setResolution(8);
   adc->adc0->setConversionSpeed(ADC_CONVERSION_SPEED::HIGH_SPEED); // change the conversion speed
   adc->adc0->setSamplingSpeed(ADC_SAMPLING_SPEED::HIGH_SPEED); // change the sampling speed 
   adc->adc0->enableInterrupts(adc0_isr);
   //adc->adc0->enableCompareRange(0,255,1,1);//enable the compare function to a range insize 0 and 255 (8 bits)
   //adc->adc0->setReference(ADC_REFERENCE::REF_3V3);//set voltage reference
   
-  //writeWavHeader();
+  writeWavHeader();
 
   adc->adc0->startContinuous(readPin);
-  delay(1000);
   StartTime = millis();
   
   
@@ -85,67 +81,22 @@ void setup()
 
 char c=0;
 
-void loop()
-{
-  //adcResult = adc->adc0->analogRead(readPin); //going to give a value between 0 and 1023
-  //wavFile.println(adcResult);
-  //Serial.println(adcResult*3.3/255);
+void loop(){
+  
   if(millis() - StartTime > 1000){
     Serial.println("entered second if statement");
       adc->adc0->stopContinuous();
 
-
-      
-//  Serial.println("Chunk2Size = ");
-//  Serial.println(subChunk2Size);
-//      wavFile.seek(40);
-//      wavFile.write((byte*)&subChunk2Size,4);
-//      Serial.print("subChunk2Size = ");
-//      Serial.println(subChunk2Size);
-  //Serial.println("Through subChunk2 write");
-
-//      wavFile.seek(4);
-//      chunkSize = 36 + subChunk2Size;
-//      wavFile.write((byte*)&chunkSize,4); 
-//      Serial.print("chunkSize = ");
-//      Serial.println(chunkSize);
-//    
-      //writeDataToWavFile();
       wavFile.close();
       Serial.println("Conversion closed, write ended");
       exit(0);
     }
-//    if(millis() - StartTime2 < 1000){
-//      ++Count;
-//      }
-//    else{
-//      Serial.println(Count);
-//      Count = 0;
-//      StartTime2 = millis();
-//      }
-    //delayMicroseconds(22.6757);//delay period for 44.1kHz sampling
-    //delay(100);
-
-    
-  //int data = getSomeData();
-//  if(Serial.available()){
-//    Serial.println("Entered first if statement serial.available");
-//    c = Serial.read();
-//    if(c =='s'){
-//      Serial.println("entered second if statement c==s");
-//      adc->adc0->stopContinuous();
-//
-//    
-//      //writeDataToWavFile();
-//      wavFile.close();
-//      Serial.println("Conversion closed, write ended");
-//    }
     
     
 }
 
-void writeWavHeader()
-{
+void writeWavHeader(){
+  
    Serial.println("Entered writeWavHeader");
    wavFile.seek(0);
    wavFile.write(chunkID,4);
@@ -164,23 +115,22 @@ void writeWavHeader()
 }
 
 
-void writeDataToWavFile(int data)
-{
-  //Serial.println("entered write function");
-  //int8_t sampleValue = map(data, 0, 255,0,255);
-  //int dataSize = dataString.legnth
-  
-  //subChunk2Size += numChannels * bitsPerSample/8 ;
-  //Serial.println("Through chunkSize write");
-  //Serial.println(sampleValue);
-  //wavFile.seek(wavFile.size()-1);
-  //wavFile.write(data,1);
-
-  //Serial.println("Through data write");
-}
-
-void adc0_isr(void){
+void adc0_isr(void){ //interrupt for when a adc read is available
   adcResult = adc->adc0->analogReadContinuous();//gives most recent adc conversion
+
+  int8_t sampleValue = map(adcResult, 0, 255, 0, 255);
+  subChunk2Size += numChannels * bitsPerSample/8;
+  wavFile.seek(40);
+  wavFile.write((byte*)&subChunk2Size,4);
+
+  wavFile.seek(4);
+  chunkSize = 36+subChunk2Size;
+  wavFile.write((byte*)&chunkSize,4);
+
+  wavFile.seek(wavFile.size() - 1);
+  wavFile.write((byte*)&sampleValue,1);
+
+
   
   //writeDataToWavFile(adcResult);//write value to adc result
 //  
@@ -194,15 +144,15 @@ void adc0_isr(void){
 //  //Serial.println(adcResult);
 //  //String temp = String(adcResult,BIN);
 //  //wavFile.seek(wavFile.size()-1);
-  wavFile.println(adcResult);//write each adc result into wav file
+//  wavFile.println(adcResult);//write each adc result into wav file
 ////  ++numSample;
-  if(millis() - StartTime2 < 1000){
-    ++Count;
-    }
-  else{
-    Serial.println(Count);
-    Count = 0;
-    StartTime2 = millis();
-    }
+//  if(millis() - StartTime2 < 1000){
+//    ++Count;
+//    }
+//  else{
+//    Serial.println(Count);
+//    Count = 0;
+//    StartTime2 = millis();
+//    }
 //  //Serial.println("ADC conversion complete");
   }
